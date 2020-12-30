e9b31215a65e18044020c6c5a186c2427223feed
Yao Chunlin <chunlinyao@gmail.com>
2020-12-30 09:54:18 +0800

    add jxyj test

--- src/main/resources/etc/cas/attributes.groovy
+++ src/main/resources/etc/cas/attributes.groovy
@@ -3,5 +3,5 @@ import java.util.*
 def Map<String, List<Object>> run(final Object... args) {
 	def (username, attributes, logger, properties, appContext) = args
 	logger.info("[{}]: The received username is [{}]", this.class.simpleName, username)
-	return [username: [username], likes: ['banana'], another:'foobar']
+	return [username: [username], user: username, likes: ['banana'], another:'foobar']
 }
